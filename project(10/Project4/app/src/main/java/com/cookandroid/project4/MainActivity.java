813849d9b27cdded4cf78de4b057989a1ff71cd1
JongUk Jeong <jongukjeong@gmail.com>
2020-08-12 09:37:13 +0900

    first upload
    first upload

---
+++ project(10/Project4/app/src/main/java/com/cookandroid/project4/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cookandroid.project4;
+
+import android.graphics.Color;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
